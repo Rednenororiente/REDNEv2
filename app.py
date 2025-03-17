912ee56f8144efd7a975c7e6aa2dc891c1e87a53
Rednenororiente <proyectodegradocps@gmail.com>
2025-03-17 14:29:54 -0500

    Update app.py

--- app.py
+++ app.py
@@ -214,7 +214,9 @@ def generate_helicorder_route():
 
 # Punto de entrada del servidor Flask
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    # Utiliza el puerto proporcionado por Render
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    #app.run(host='0.0.0.0', port=5000)
 
 
 
